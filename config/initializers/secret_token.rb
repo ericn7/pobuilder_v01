e1c38e1719fbac9aac1526359e185a54989870c0
Eric Nelson <eric.mckenzie.nelson@gmail.com>
2011-07-04 00:58:31 -0700

    Initia commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PobuilderV01::Application.config.secret_token = 'b101e84cfacf092a670ea922afc8ee77ac415e30aa894cfc6afee1abf7c5af778e97e2f8457e2eaf73f4b4994843dd33a85aad6d2b9d0dbe22281c9699a3e73c'
